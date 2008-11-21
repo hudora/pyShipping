218d143de9964d6f2f6dcc03f9a8d2f317f3a2f1
m.dornseif@hudora.de <m.dornseif@hudora.de@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2008-11-21 07:39:35 +0000

    Now in all circumstances known to me the files are installed BugzID: 4637
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/projects/pyShipping/trunk@4229 2a7d6d32-97f8-0310-8fd3-c67d3a265405

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 setup(name='pyShipping',
       maintainer='Maximillian Dornseif',
       maintainer_email='md@hudora.de',
-      version='1.1',
+      version='1.1a',
       description='pyShipping - Shipping related Toolkit',
       long_description=long_description,
       classifiers=['License :: OSI Approved :: BSD License',
@@ -20,7 +20,7 @@ setup(name='pyShipping',
       # download_url
       zip_safe=False,
       packages=find_packages(),
-      package_data={'': ['pyshipping/carriers/dpd/georoutetables/*.gz']},
+      include_package_data=True,
       # install_requires = ['huLOG'],
       dependency_links = ['http://cybernetics.hudora.biz/dist/', 'http://cybernetics.hudora.biz/nonpublic'],
 )

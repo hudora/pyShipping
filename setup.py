65f82e5780060ffe53675d4a4e6cd1f08e943108
m.dornseif@hudora.de <m.dornseif@hudora.de@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2008-12-02 21:34:05 +0000

    Grundlagen Furu die Berechnung der Bündelfähigkeit. Siehe #5071 und http://reviewboard.hudora.biz/r/142/
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/projects/pyShipping/trunk@4395 2a7d6d32-97f8-0310-8fd3-c67d3a265405

--- setup.py
+++ setup.py
@@ -21,7 +21,5 @@ setup(name='pyShipping',
       zip_safe=False,
       packages=find_packages(),
       include_package_data=True,
-      # install_requires = ['huLOG'],
-      dependency_links = ['http://cybernetics.hudora.biz/dist/', 'http://cybernetics.hudora.biz/nonpublic'],
 )
 

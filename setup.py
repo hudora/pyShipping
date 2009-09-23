62d2f6560109cbe98b8486a9e9cefc9891544ec7
c.borgolte@hudora.de <c.borgolte@hudora.de@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2009-09-23 09:17:10 +0000

    Provide a dirname to fortras.bordero.ship(), where the generated bordero file will be saved. This is more flexible and allows easier testing.
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/projects/pyShipping/trunk@6576 2a7d6d32-97f8-0310-8fd3-c67d3a265405

--- setup.py
+++ setup.py
@@ -3,15 +3,13 @@ pyShipping provides connetions to interface with shipping companier and to trans
 information.
 """
 
-from ez_setup import use_setuptools
-use_setuptools()
 from setuptools import setup, find_packages
 
 
 setup(name='pyShipping',
       maintainer='Maximillian Dornseif',
       maintainer_email='md@hudora.de',
-      version='1.3',
+      version='1.3p1',
       description='pyShipping - Shipping related Toolkit',
       long_description=long_description,
       classifiers=['License :: OSI Approved :: BSD License',

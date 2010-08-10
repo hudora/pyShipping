32d992786a3f9f060a07f6f74dbf410fbbf7f144
Maximillian Dornseif <md@hudora.de>
2010-08-10 22:36:23 +0200

    slight API change, buendelung(), addition and hat_gleiche_seiten() implemented

--- pyshipping/package.py
+++ pyshipping/package.py
@@ -30,7 +30,25 @@ class PackageSize(object):
             self.heigth, self.width, self.length = size
         (self.heigth, self.width, self.length) = sorted((int(self.heigth), int(self.width),
                                                          int(self.length)), reverse=True)
-    
+
+    def __getitem__(self, key):
+        """The coordinates can be accessed as if the object is a tuple. 
+        >>> p = PackageSize((500, 400, 300))
+        >>> p[0]
+        500
+        """
+        if key == 0:
+            return self.heigth
+        if key == 1:
+            return self.width
+        if key == 2:
+            return self.length
+        if isinstance(key, tuple):
+            return (self.heigth, self.width, self.length)[key[0]:key[1]]
+        if isinstance(key, slice):
+            return (self.heigth, self.width, self.length)[key]
+        raise IndexError
+
     def _get_volume(self):
         """Calculates the volume of a PackageSize object.
             
@@ -58,6 +76,13 @@ class PackageSize(object):
         return maxdimension + 2 * (sum(otherdimensions))
     gurtmass = property(_get_gurtmass)
     
+    def hat_gleiche_seiten(self, other):
+        """Prüft, ob other mindestens eine gleich grosse Seite mit self hat."""
+        
+        meineseiten = set([(self.heigth, self.width), (self.heigth, self.length),(self.heigth, self.length)])
+        otherseiten = set([(other.heigth, other.width), (other.heigth, other.length),(other.heigth, other.length)])
+        return not meineseiten.isdisjoint(otherseiten)
+
     def __eq__(self, other):
         """PackageSize objects are equal if they have exactly the same dimensions.
            
@@ -80,7 +105,31 @@ class PackageSize(object):
            """
 
         return PackageSize((self.heigth, self.width, self.length*multiplicand))
-    
+
+    def __add__(self, other):
+        """
+            >>> PackageSize((1600, 250, 480)) + PackageSize((1600, 470, 480))
+            <PackageSize 1600x720x480>
+            >>> PackageSize((1600, 250, 480)) + PackageSize((1600, 480, 480))
+            <PackageSize 1600x730x480>
+            >>> PackageSize((1600, 250, 480)) + PackageSize((1600, 490, 480))
+            <PackageSize 1600x740x480>
+            """
+        
+        meineseiten = set([(self.heigth, self.width), (self.heigth, self.length),(self.heigth, self.length)])
+        otherseiten = set([(other.heigth, other.width), (other.heigth, other.length),(other.heigth, other.length)])
+        if meineseiten.isdisjoint(otherseiten):
+            raise ValueError("%s has no fitting sites to %s" % (self, other))
+        candidates = sorted(meineseiten.intersection(otherseiten), reverse=True)
+        stack_on = candidates[0]
+        mysides = [self.heigth, self.width, self.length]
+        mysides.remove(stack_on[0])
+        mysides.remove(stack_on[1])
+        othersides = [other.heigth, other.width, other.length]
+        othersides.remove(stack_on[0])
+        othersides.remove(stack_on[1])
+        return PackageSize((stack_on[0], stack_on[1], mysides[0] + othersides[0]))
+
     def __str__(self):
         return "%dx%dx%d" % (self.heigth, self.width, self.length)
     
@@ -88,7 +137,7 @@ class PackageSize(object):
         return "<PackageSize %dx%dx%d>" % (self.heigth, self.width, self.length)
 
 
-class Package:
+class Package(PackageSize):
     """Represents a package as used in cargo/shipping aplications."""
     
     def __init__(self, size=None, weight=None):
@@ -97,23 +146,59 @@ class Package:
         Size can be the 3 dimensions of a package in mm. Note that we only support box-shaped packages. Size
         may be given as a tuple of integers or a string in th eformat '111x222x333'. See PackageSize.__init__
         for further details."""
-        
-        self.size = size
         self.weight = weight
-        
-        if size:
-            self.size = PackageSize(size)
-            
+        PackageSize.__init__(self, size)
+
     def __mul__(self, multiplicand):
-        return Package(str(self.size*multiplicand), self.weight*multiplicand)
-    
+        return Package((self.heigth, self.width, self.length*multiplicand), self.weight*multiplicand)
+
     def __str__(self):
-        ret = ['Package']
-        if self.size:
-            ret.append(unicode(self.size))
-        if self.weight:
-            ret.append(unicode(self.weight))
-        return ', '.join(ret)
+        return "<Package %dx%dx%d %d>" % (self.heigth, self.width, self.length, self.weight)
+
+
+def buendelung(kartons, maxweight=32000, maxgurtmass=3000):
+    """Versucht Pakete so zu bündeln, so dass das Gurtmass nicht überschritten wird.
+    
+    Gibt die gebündelten Pakete und die Zahl der benötigten Bündelungsvorgänge zurück.
+    
+    >>> buendelung([Package((800, 310, 250)), Package((800, 310, 250)), Package((800, 310, 250)), Package((800, 310, 250))])
+    ([<PackageSize 800x560x500>, <PackageSize 800x310x250>], 1)
+    >>> buendelung([Package((800, 310, 250)), Package((800, 310, 250)), Package((800, 310, 250)), Package((800, 310, 250)), Package((450, 290, 250)), Package((450, 290, 250))])
+    ([<PackageSize 800x560x500>, <PackageSize 800x310x250>, <PackageSize 500x450x290>], 2)
+    """
+    
+    if not kartons:
+        return kartons
+    ret = []
+    lastkarton = kartons.pop(0)
+    buendel = False
+    buendelcounter = 0
+    while kartons:
+        currentcarton = kartons.pop(0)
+        # check if 2 dimensions fit
+        if currentcarton.hat_gleiche_seiten(lastkarton):
+            # new carton has the same size in two dimensions and the sum of both in the third
+            newcarton = Package(lastkarton[:2] + tuple([currentcarton[2] + lastkarton[2]]))
+            # gurtmass pruefen
+            if newcarton.gurtmass < maxgurtmass:
+                # ok, we can bundle
+                lastkarton = newcarton
+                if buendel is False:
+                    buendelcounter += 1
+                buendel = True
+            else:
+                # too big
+                ret.append(lastkarton)
+                lastkarton = currentcarton
+                buendel = False
+        else:
+            # different sizes, ditch for now
+            ret.append(lastkarton)
+            lastkarton = currentcarton
+            buendel = False
+    ret.append(lastkarton)
+    return ret, buendelcounter
+
 
 
 ### Tests
@@ -158,17 +243,16 @@ class PackageSizeTests(unittest.TestCase):
     
 
 if __name__ == '__main__':
-    
+
     factor = 0
     while True:
         factor += 1
         single = Package((750, 240, 220), 7400)
         multi = single * factor
-        if multi.weight > 31000 or multi.size.gurtmass > 3000:
+        if multi.weight > 31000 or multi.gurtmass > 3000:
             multi = single * (factor - 1)
-            print factor - 1, multi, multi.size.gurtmass
+            #print factor - 1, multi, multi.gurtmass
             break
-            
-            
+
     doctest.testmod()
     unittest.main()

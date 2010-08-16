35209dc0016ff76b000ad45f4c40154ce0533ae8
Maximillian Dornseif <md@hudora.de>
2010-08-16 08:58:03 +0200

    First Binpacking implementation in pure Python

---
+++ pyshipping/binpack.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+binpack.py
+
+Created by Maximillian Dornseif on 2010-08-16.
+Copyright (c) 2010 HUDORA. All rights reserved.
+"""
+
+from binpack_3dbpp import *
--- pyshipping/binpack_3dbpp.pyx
+++ pyshipping/binpack_3dbpp.pyx
@@ -9,9 +9,9 @@ cdef extern:
 
 def binpack(binsize, sizes, nodelimit=5000, iterlimit=5000, timelimit=2, robotpackable=False):
     cdef int numelements, box_w, box_h, box_d
-    cdef int w_input[500], h_input[500], d_input[500]
-    cdef int w_output[500], h_output[500], d_output[500]
-    cdef int boxno[500], lowerbound, upperbound
+    cdef int w_input[100], h_input[100], d_input[100]
+    cdef int w_output[100], h_output[100], d_output[100]
+    cdef int boxno[100], lowerbound, upperbound
     # cdef int nodelimit, int iterlimit, int timelimit
     cdef int nodeused, iterused, timeused
     cdef int packingtype
@@ -20,7 +20,7 @@ def binpack(binsize, sizes, nodelimit=5000, iterlimit=5000, timelimit=2, robotpa
         packingtype = 1
     numelements = len(sizes)
     box_w, box_h, box_d = sorted([int(x) for x in binsize])
-    sizes = sizes[:500]
+    sizes = sizes[:100]
     for i, box in enumerate(sorted([sorted([int(x) for x in m]) for m in sizes], reverse=True)):
         w_input[i], h_input[i], d_input[i] = box
 
